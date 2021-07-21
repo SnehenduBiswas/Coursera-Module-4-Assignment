85001d9ac7e7bf81405193f4df113885c471dffe
SnehenduBiswas <65412979+SnehenduBiswas@users.noreply.github.com>
2021-07-21 20:32:51 +0530

    Update SayGoodBye.js

--- module4-solution/SayGoodBye.js
+++ module4-solution/SayGoodBye.js
@@ -1,8 +1,8 @@
 (function(window) {
     var bye_Speaker = new Object();
-    var Word = "Good Bye";
+    var word = "Good Bye";
     bye_Speaker.speak = function speak(name) {
-        console.log(Word + " " + name);
+        console.log(word + " " + name);
     };
     window.bye_Speaker = bye_Speaker;
 })(window);
